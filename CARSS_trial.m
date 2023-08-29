ced94e051d316aad26463ca66d6424654392f696
Teja Mannepalli <46516739+teja-mannepalli@users.noreply.github.com>
2023-08-29 16:46:14 -0500

    Update CARSS_trial.m

--- CARSS_trial.m
+++ CARSS_trial.m
@@ -8,6 +8,6 @@
 % [locs,peak_indxs_max,peak_indxs_min,Ds,Delec] = Stage_0(K,elec.chanpos,grid.dipos,r,Ds);
 %%
 % [J_CARSS,peaks,pos_sources_all,J_sLOR]...
-    = CARSS(K,phi,gd,locs,peak_indxs_max,peak_indxs_min,Ds);
+%    = CARSS(K,phi,gd,locs,peak_indxs_max,peak_indxs_min,Ds);
 
 % % % % [~,locs] = find_locs(Delec,elec,db);
