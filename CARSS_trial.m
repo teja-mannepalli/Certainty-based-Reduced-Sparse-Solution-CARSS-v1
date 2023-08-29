13b039e759f0c4cd3dcafa617295c7db4b5b9668
Teja Mannepalli <46516739+teja-mannepalli@users.noreply.github.com>
2023-08-29 16:45:16 -0500

    Update CARSS_trial.m

--- CARSS_trial.m
+++ CARSS_trial.m
@@ -1,34 +1,13 @@
-clc
-close all;
-clear;
-
-load('grid.mat');
-load('K.mat');
-load('elec.mat');
-
 %%
-[Ds] = find_Ds(grid);
+% Use as
 %%
-r = 85; % in mm
-fprintf('Stage-0');
-fprintf('\n');
-[locs,peak_indxs_max,peak_indxs_min,Ds,Delec] = Stage_0(K,elec.chanpos,grid.dipos,r,Ds);
-% [~,locs] = find_locs(Delec,elec,db);
+% [Ds] = find_Ds(grid);
 %%
-close all;
-clc
-aa = [45];  
-% 1345, 345, 45
-gd = 20;
-
-phi = sum(K(:,aa),2);
-
-J_problem = zeros(size(K,2),1);
-J_problem(aa,1) = 1;
-
-fprintf('CARSS');
-fprintf('\n');
-
-[J_CARSS,peaks,pos_sources_all,J_sLOR]...
+% fprintf('Stage-0');
+% fprintf('\n');
+% [locs,peak_indxs_max,peak_indxs_min,Ds,Delec] = Stage_0(K,elec.chanpos,grid.dipos,r,Ds);
+%%
+% [J_CARSS,peaks,pos_sources_all,J_sLOR]...
     = CARSS(K,phi,gd,locs,peak_indxs_max,peak_indxs_min,Ds);
 
+% % % % [~,locs] = find_locs(Delec,elec,db);
